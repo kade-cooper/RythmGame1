0484bb501ffd6ec2b590c310d8b35a3c8a9655e0
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2023-02-20 16:10:31 -0500

    fixed sword

--- Assets/scripts/swordSlash.cs
+++ Assets/scripts/swordSlash.cs
@@ -7,7 +7,7 @@ public class swordSlash : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 0.2f);
+        Destroy(this.gameObject, 0.1f);
     }
 
     // Update is called once per frame
