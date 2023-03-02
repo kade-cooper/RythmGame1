b01d00deb84c4fa4fc089969d05fa67ee864b0f3
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2023-03-02 18:30:30 -0500

    gave "sword slash" a speed and made it invisible

--- Assets/scripts/swordSlash.cs
+++ Assets/scripts/swordSlash.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class swordSlash : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 0.1f);
+        Destroy(this.gameObject, 0.15f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 newPosition = this.transform.position + new Vector3(speed*Time.deltaTime, 0f, 0f);
+        this.transform.position = newPosition;
     }
     
 }
